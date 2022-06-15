ff88cc822f6d8f49914b2b54e525ffc52aa4bab8
AnderSpidy <anderson.rs1996@outlook.com>
2022-06-15 16:49:57 -0300

    implementado metodo multiplicar

--- Calculadora_Tabajara.ConsoleApp2/Operacoes.cs
+++ Calculadora_Tabajara.ConsoleApp2/Operacoes.cs
@@ -14,5 +14,10 @@ namespace Calculadora_Tabajara.ConsoleApp2
             int resultado = numero1 + numero2;
             return resultado;
         }
+        public int Multiplicar(int numero1, int numero2)
+        {
+            int resultado = numero1 * numero2;
+            return resultado;
+        }
     }
 }
